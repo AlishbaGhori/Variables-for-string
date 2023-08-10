d0fc67823f173473d7803ec40e8976db66e61463
AlishbaGhori <126356642+AlishbaGhori@users.noreply.github.com>
2023-08-10 15:46:00 -0700

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,63 @@
+ var userName = "Irfan";
+ var myName = "Alishba ";
+ alert(myName + userName);
+
+////////////////
+
+var message = "HELLO WORLD";
+alert(message);
+
+////////////////////
+
+var age = "20";
+alert(age + " " + "years old");
+
+/////////////////////
+
+var sentence = "course";
+alert("Certified Mobile Aplication Development");
+
+////////////////
+
+// var PIZZA = "break the word " ;
+// console.log("PIZZA");
+// console.log("PIZZ");
+// console.log("PIZ");
+// console.log("PI");
+// console.log("P");
+
+
+var PIZZA = "pizza";
+
+alert("PIZZA\n" +
+PIZZA.slice(0 , 4) + "\n" +
+PIZZA.slice(0 , 3) + "\n" +
+PIZZA.slice(0 , 2) + "\n" +
+PIZZA.slice(0 ,1));
+
+//////////////////////
+
+var email = "abc@gmail.com";
+alert("My email address is " + email);
+
+//////////////////////
+
+var book = " A smarter way to learn Javascript";
+alert("I am trying to learn from the book A smarter way to learn JavaScript");
+
+/////////////////
+
+var message = "hello";
+document.write = "YAH ! I can write HTML content through JavaScript";
+
+///////////////
+
+var myString = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+
+alert(myString);
+
+document.write(myString);
+
+
+
+ 
